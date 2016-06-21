4c4aa96d1a6d8da28501e9810bf15034f4b42772
Quentin <blueherb@hotmail.fr>
2016-06-21 17:13:37 +0200

    Liaison des pages (ça va chier !)

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
-<?php session_start();
-//session_destroy(); 
-include_once 'Config.php';?>
+<?php
+session_start();
+session_destroy(); 
+include_once 'Config.php';
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -8,44 +10,49 @@ include_once 'Config.php';?>
         <meta charset="UTF-8">
         <link href="style.css" rel="stylesheet" type="text/css"/>
         <!--import javascript-->
-            <!--import de la bibliotheque jQuery pour les animations-->
-            <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+        <!--import de la bibliotheque jQuery pour les animations-->
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
         <!--script javascript-->
-            <!--script js de la fonction easing de jQuery non incluse dans la bibliotheque par defaut-->
-            <script type="text/javascript" src="scripts/jquery.easing.1.3.js"></script>
-            <!--script js de la fonction softScroll pour les ancres-->
-            <script type="text/javascript" src="scripts/scroll.js"></script>
+        <!--script js de la fonction easing de jQuery non incluse dans la bibliotheque par defaut-->
+        <script type="text/javascript" src="scripts/jquery.easing.1.3.js"></script>
+        <!--script js de la fonction softScroll pour les ancres-->
+        <script type="text/javascript" src="scripts/scroll.js"></script>
     </head>
-    
+
     <body id="top"> 
-    <?php
-    try{
-        $bdd = new PDO('mysql:host='.Config::SERVERNAME.';dbname='.Config::DBNAME.';charset=utf8', Config::LOGIN, '');
+        <?php
+        try {
+            $bdd = new PDO('mysql:host=' . Config::SERVERNAME . ';dbname=' . Config::DBNAME . ';charset=utf8', Config::LOGIN, '');
+        } catch (Exception $e) {
+            die('Erreur : ' . $e->getMessage());
         }
-    catch (Exception $e){
-        die('Erreur : ' . $e->getMessage());
-        }
-    ?>
-    
-    <h1>Interface d'étude de prélèvement</h1>
-        
-    <?php if(isset($_SESSION['logged']) == true){//GESTION DE LA SESSION, LE CHERCHEUR EST LOGGUE, PAS LE PRELEVEUR
-            echo'<a href="interchercheur.php" class="bouton">Accès préleveur</a>';
-         }
-            else{echo'<a href="login.php" class="bouton">Connexion</a>';
-         } 
         ?>
-    
-    <form action="interprel.php" method="post">
-        <label for="nom-projet">Projet :</label>
-        <input list="projet" id="nom-projet">
-        <?php //REQUETE POUR LA LISTE DES PROJETS EN COURS
-        $reponse = $bdd->query("SELECT Nom FROM plage");
-        echo "<datalist id=\"projet\">"; 
-            while ($donnees = $reponse->fetch()) {
-                echo "<option>".$donnees['Nom']."</option>";}
-        echo "</datalist>";  
-        $reponse->closeCursor();?>
+
+        <h1>Interface d'étude de prélèvement</h1>
+
+        <?php
+        if (isset($_SESSION['logged']) == true) {//GESTION DE LA SESSION, LE CHERCHEUR EST LOGGUE, PAS LE PRELEVEUR
+            echo'<a href="interchercheur.php" class="bouton">Accès préleveur</a>';
+        } else {
+            echo'<a href="login.php" class="bouton">Connexion</a>';
+        }
+        ?>
+
+        <form action="index.php" method="post">
+            <label for="nom">Projet :</label>
+            <select name="idprojet" id="nom">
+                <?php
+                //LISTE DES PROJETS EN COURS, ON RECUPERE L'ID POUR L'ENVOYER DANS LA REQUETE SUIVANTE
+                $reponse = $bdd->query("SELECT ID,Nom FROM plage");
+                while ($donnees = $reponse->fetch()) {
+                    echo "<option value='" . $donnees['ID'] . "'>" . $donnees['Nom'] . "</option>";
+                }
+                $reponse->closeCursor();
+                ?>
+            </select>
+            <input type="submit" value="Envoyer" id="envoiprojet" name="envoiprojet"/>
+        </form>
+
 
         <table>
             <caption><h2>Groupe(s) assigné(s) au projet</h2></caption>
@@ -62,25 +69,34 @@ include_once 'Config.php';?>
                 </tr>
             </tfoot>
             
-            <?php //AFFICHE LES GROUPES DEJA CREES SUIVANT LE PROJET SELECTIONNE
-            $reponse = $bdd->query("SELECT Nom FROM zones WHERE IDplage = (SELECT ID FROM plage WHERE Nom ='Crevette-sur-salade')");
-            while ($donnees = $reponse->fetch())
-            {
-               echo"<tr>";
-               echo"<td>".$donnees['Nom']."</td>";
-               if(isset($_SESSION['logged']) == true){ //LE CHERCHEUR PEUT TERMINER UN PRELEVEMENT
-                   echo'<td><input type="submit" id="complete" class="bouton" name="complete" value="Clore"/></td>';
-               }
-               else{ //LE PRELEVEUR PEUT COMPLETER SON PRELEVEMENT
-                   echo'<td><input type="submit" id="complete" class="bouton" name="complete" value="Compléter"/></td>';
-               }
-               echo"</tr>";
-            }$reponse->closeCursor();?>
+                <?php
+                if (isset($_POST['envoiprojet']) == "Envoyer") {
+                    //AFFICHE LES GROUPES DEJA CREES SUIVANT LE PROJET SELECTIONNE
+                    $reponse = $bdd->query("SELECT ID,Nom FROM zones WHERE IDplage = (SELECT ID FROM plage WHERE id=" . $_POST['idprojet'] . ")");
+                    while ($donnees = $reponse->fetch()) {
+                        echo"<tr>";
+                        echo"<td>" . $donnees['Nom'] . "</td>";
+                        if (isset($_SESSION['logged']) == true) { //LE CHERCHEUR PEUT TERMINER UN PRELEVEMENT
+                            echo'<form action="interprel.php" method="post">';
+                            echo'<td><input type="hidden" name="groupeid" value="'. $donnees['ID'] .'"/>';
+                            echo'<input type="submit" class="bouton" value="Clore"/></td>';
+                            echo'</form>';
+                        } else { //LE PRELEVEUR PEUT COMPLETER SON PRELEVEMENT
+                            echo'<form action="interprel.php" method="get">';
+                            echo'<td><input type="hidden" name="groupeid" value="'. $donnees['ID'] .'"/>';
+                            echo'<input type="submit" class="bouton" value="Completer"/></td>';
+                            echo'</form>';
+                        }
+                        echo"</tr>";
+                    }$reponse->closeCursor();
+                }
+                ?>
+            
         </table>
-    </form>
-    
-   <?php if(isset($_SESSION['logged']) == false){
-        echo'
+
+        <?php
+        if (isset($_SESSION['logged']) == false) {
+            echo'
         <form action="traitementGPS.php" method="post">
             <h2>Inscription d\'un nouveau groupe</h2>
             <p>
@@ -190,9 +206,10 @@ include_once 'Config.php';?>
             <input type="submit" id="cree" class="bouton" name="cree" value="Créer"/>
             <input type="reset" class="bouton" value="Vider"/>
         </form>';
-        } ?>
-    <footer>
-        <a href="#top" class="bouton" title="Haut de page"><img src="images/icone_fleche-retour.png" alt="Haut de page"/></a>
-    </footer>
+        }
+        ?>
+        <footer>
+            <a href="#top" class="bouton" title="Haut de page"><img src="images/icone_fleche-retour.png" alt="Haut de page"/></a>
+        </footer>
     </body>
 </html>
--- interprel.php
+++ interprel.php
@@ -1,115 +1,139 @@
-<?php //VARIABLES TEMPORAIRES --> prélèvement de la zone 1 de la ville 1 avec le groupe 1
+<?php
+//VARIABLES TEMPORAIRES --> prélèvement de la zone 1 de la ville 1 avec le groupe 1
 include_once 'Config.php';
-$IDgroupe = Config::GROUPE;
-$IDzone = Config::ZONE;
-$IDplage = Config::PLAGE; ?>
+echo $_GET['groupeid'];
+        //CONNEXION BDD
+        try {
+            $bdd = new PDO('mysql:host=' . Config::SERVERNAME . ';dbname=' . Config::DBNAME . ';charset=utf8', Config::LOGIN, '');
+        } catch (Exception $e) {
+            die('Erreur : ' . $e->getMessage());
+        }
+        $reponse = $bdd->query("SELECT Nom FROM zones WHERE ID=" . $_GET['groupeid']); //REQUETE POUR LE GROUPE EN COURS
+        $donnees = $reponse->fetch(); ?>
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Interface Préleveur - "nom du projet via Php"</title>
+        <title>Interface Préleveur - <?php echo $donnees['Nom']; ?></title>
         <meta charset="UTF-8">
         <link href="style.css" rel="stylesheet" type="text/css"/>
         <!--import javascript-->
-            <!--import de la bibliotheque jQuery pour les animations-->
-            <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+        <!--import de la bibliotheque jQuery pour les animations-->
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
         <!--script javascript-->
-            <!--script js de la fonction easing de jQuery non incluse dans la bibliotheque par defaut-->
-            <script type="text/javascript" src="scripts/jquery.easing.1.3.js"></script>
-            <!--script js de la fonction softScroll pour les ancres-->
-            <script type="text/javascript" src="scripts/scroll.js"></script>
+        <!--script js de la fonction easing de jQuery non incluse dans la bibliotheque par defaut-->
+        <script type="text/javascript" src="scripts/jquery.easing.1.3.js"></script>
+        <!--script js de la fonction softScroll pour les ancres-->
+        <script type="text/javascript" src="scripts/scroll.js"></script>
 
-            <!--script js de la fonction afficher-cacher-->
-            <script type="text/javascript" src="scripts/afficher-cacher.js"></script>
+        <!--script js de la fonction afficher-cacher-->
+        <script type="text/javascript" src="scripts/afficher-cacher.js"></script>
     </head>
-    
-    <body id="top">
-    <?php //CONNEXION BDD
-    try{
-        $bdd = new PDO('mysql:host='.Config::SERVERNAME.';dbname='.Config::DBNAME.';charset=utf8', Config::LOGIN, '');
-    }
-    catch (Exception $e){
-            die('Erreur : ' . $e->getMessage());
-    }
-    ?>
-    <?php $reponse = $bdd->query("SELECT Nom FROM zones WHERE ID=".$IDzone); //REQUETE POUR LE GROUPE EN COURS
-    $donnees = $reponse->fetch();
-    echo "<h1>Interface Préleveur - ".$donnees['Nom']."</h1>"; ?>
-    
-    <span class="bouton" id="bouton_plage" onclick="javascript:afficher_cacher('plage');">
-        Cacher les informations de la plage
-    </span>
-    <script type="text/javascript">
-    //<!-- cacher une partie de la page (menu infos)
-        afficher_cacher('plage');
-    //-->
-    </script>
-    <div id="plage">
-        <div id="infos-projet">
-            <?php //REQUETE POUR RECUPERER LES INFORMATIONS DU PROJET (ville, nom projet, date...)
-            $reponse = $bdd->query("SELECT Nom,Ville,Datepreleve FROM plage WHERE ID=".$IDplage);
-            $donnees = $reponse->fetch();
-            echo "<p>Projet: ".$donnees['Nom']."</p>";
-            echo "<p>Ville: ".$donnees['Ville']."</p>";
-            echo "<p>Date: ".$donnees['Datepreleve']."</p>"; 
-            $reponse->closeCursor();?>
-        </div>
-        <div id="coordonnees" class="clear">
-            <?php //REQUETE POUR AFFICHER L'ESPACE DE TRAVAIL (coords, superficie)
-            $reponse = $bdd->query("SELECT latA,longA,latB,longB,latC,longC,latD,longD,Superficie "
-                    . "FROM Zones WHERE ID=".$IDgroupe);
-            $donnees = $reponse->fetch();
-            echo "<div class=\"couleur0-0\"> ".$donnees['latA']." ".$donnees['longA']."</div>";
-            echo "<div class=\"couleur0-1\"> ".$donnees['latB']." ".$donnees['longB']."</div>";
-            echo"<div>Surface : ".$donnees['Superficie']." m²</div>";
-            echo "<div class=\"couleur1-0\"> ".$donnees['latC']." ".$donnees['longC']."</div>";
-            echo "<div class=\"couleur1-1\"> ".$donnees['latD']." ".$donnees['longD']."</div>"; 
-            $reponse->closeCursor();?>
-        </div>
-    </div>
-    
-    <form method="post" action="traitement.php">
-            
-        <table>
-            <caption><h2>Prélèvement de la zone</h2></caption>  
-            <thead><!--en tete de tableau-->
-                <tr>
-                    <th>Espèces</th>
-                    <th>Nombres</th>
-                    <th>Fonctions</th>
-                </tr>
-            </thead>
-            <tfoot><!--pied de tableau-->
-                <tr>
-                    <th>Espèces</th>
-                    <th>Nombres</th>
-                    <th>Fonctions</th>
-                </tr>
-            </tfoot>
-            <tbody><!--corp du tableau-->
-                <tr>
-                    <td><input type="text" name="mob" id="mob" placeholder="Choisissez un nom" required/></td>
-                    <td><input type="number" min="0" step="1" name="nb" id="nb" placeholder="Choisissez un chiffre" required/></td>
-                    <td><input type="submit" id="ajout" class="bouton" name="ajout" value="Ajouter"/></td>
-                </tr>
-                <?php //REQUETE POUR AFFICHER LA LISTE DES ESPECES TROUVE PAR LE GROUPE EN COURS
-                $reponse = $bdd->query("SELECT Nom FROM especes WHERE IDzone =".$IDzone);
-                while ($donnees = $reponse->fetch())
-                {
-                   echo"<tr>";
-                   echo"<td>".$donnees['Nom']."</td>";
-                   echo"<td>0</td>";
-                   echo'<td><input type="submit" id="modifie" class="bouton" name="modifie" value="Modifier"/></td>';
-                   echo"</tr>";
-                }$reponse->closeCursor();?>
-            </tbody>
-        </table>
 
-        <a href="index.php" class="bouton">Retour</a>
-        <input type="submit" id="cloture" class="bouton" name="cloture" value="Cloturer"/>
-    </form>
-    
-    <footer>
-        <a href="#top" class="bouton" title="Haut de page"><img src="images/icone_fleche-retour.png" alt="Haut de page"/></a>
-    </footer>
+    <body id="top">
+        <?php
+        echo "<h1>Interface Préleveur - " . $donnees['Nom'] . "</h1>";
+        ?>
+
+        <span class="bouton" id="bouton_plage" onclick="javascript:afficher_cacher('plage');">
+            Cacher les informations de la plage
+        </span>
+        <script type="text/javascript">
+            //<!-- cacher une partie de la page (menu infos)
+            afficher_cacher('plage');
+            //-->
+        </script>
+        <div id="plage">
+            <div id="infos-projet">
+                <?php
+                //REQUETE POUR RECUPERER LES INFORMATIONS DU PROJET (ville, nom projet, date...)
+                $reponse = $bdd->query("SELECT Nom,Ville,Datepreleve FROM plage WHERE ID=(SELECT IDplage FROM zones WHERE ID=".$_GET['groupeid'].")");
+                $donnees = $reponse->fetch();
+                echo "<p>Projet: " . $donnees['Nom'] . "</p>";
+                echo "<p>Ville: " . $donnees['Ville'] . "</p>";
+                echo "<p>Date: " . $donnees['Datepreleve'] . "</p>";
+                $reponse->closeCursor();
+                ?>
+            </div>
+            <div id="coordonnees" class="clear">
+                <?php
+                //REQUETE POUR AFFICHER L'ESPACE DE TRAVAIL (coords, superficie)
+                $reponse = $bdd->query("SELECT latA,longA,latB,longB,latC,longC,latD,longD,Superficie "
+                        . "FROM Zones WHERE ID=" . $_GET['groupeid']);
+                $donnees = $reponse->fetch();
+                echo "<div class=\"couleur0-0\"> " . $donnees['latA'] . " " . $donnees['longA'] . "</div>";
+                echo "<div class=\"couleur0-1\"> " . $donnees['latB'] . " " . $donnees['longB'] . "</div>";
+                echo"<div>Surface : " . $donnees['Superficie'] . " m²</div>";
+                echo "<div class=\"couleur1-0\"> " . $donnees['latC'] . " " . $donnees['longC'] . "</div>";
+                echo "<div class=\"couleur1-1\"> " . $donnees['latD'] . " " . $donnees['longD'] . "</div>";
+                $reponse->closeCursor();
+                ?>
+            </div>
+        </div>
+
+        <form method="post" action="">
+
+            <table>
+                <?php if(isset($_POST['ajout']))
+                {
+                    $req = $bdd->prepare('INSERT INTO espece (Nom,IDzone) VALUES(?, ?)');
+                    $req->execute(array($_POST['mob'], $_GET['groupeid']));
+                    $req->closeCursor();
+                    $reponse = $bdd->prepare("SELECT IDzone FROM espece ORDER BY IDzone DESC LIMIT 1");
+                    $id = $reponse->fetch();
+                    $req->closeCursor();
+                    $req = $bdd->prepare('INSERT INTO prelevement (IDzone,IDespece,quantite) VALUES(?, ?, ?)');
+                    $req->execute(array($_GET['groupeid'],$id,$_POST['nb']));
+                    $req->closeCursor();}
+                ?>
+                <caption><h2>Prélèvement de la zone</h2></caption>  
+                <thead><!--en tete de tableau-->
+                    <tr>
+                        <th>Espèces</th>
+                        <th>Nombres</th>
+                        <th>Fonctions</th>
+                    </tr>
+                </thead>
+                <tfoot><!--pied de tableau-->
+                    <tr>
+                        <th>Espèces</th>
+                        <th>Nombres</th>
+                        <th>Fonctions</th>
+                    </tr>
+                </tfoot>
+                <tbody><!--corp du tableau-->
+                    <tr>
+                        <td><input type="text" name="mob" id="mob" placeholder="Choisissez un nom" required/></td>
+                        <td><input type="number" min="0" step="1" name="nb" id="nb" placeholder="Choisissez un chiffre" required/></td>
+                        <td><input type="submit" id="ajout" class="bouton" name="ajout" value="Ajouter"/></td>
+                    </tr>
+                    <?php
+                    //REQUETE POUR AFFICHER LA LISTE DES ESPECES TROUVE PAR LE GROUPE EN COURS
+                    $reponse = $bdd->query("SELECT Nom,quantite FROM espece "
+                                            ."INNER JOIN prelevement ON prelevement.IDespece = espece.IDespeces "
+                                            ."WHERE prelevement.IDzone =".$_GET['groupeid']);
+                    if($reponse != null){
+                    while ($donnees = $reponse->fetch()) {
+                        echo"<tr>";
+                        echo"<td>" . $donnees['Nom'] . "</td>";
+                        echo"<td>". $donnees['quantite'] ."</td>";
+                        echo'<td><input type="submit" id="modifie" class="bouton" name="modifie" value="Modifier"/></td>';
+                        echo"</tr>";
+                    }$reponse->closeCursor();}
+                    else{echo"<tr>";
+                        echo"<td>Il n'y a encore aucune espèce inscrite</td>";
+                        echo"<td></td>";
+                        echo'<td></td>';
+                    echo"</tr>";}
+                    ?>
+                </tbody>
+            </table>
+
+            <a href="index.php" class="bouton">Retour</a>
+            <input type="submit" id="cloture" class="bouton" name="cloture" value="Cloturer"/>
+        </form>
+
+        <footer>
+            <a href="#top" class="bouton" title="Haut de page"><img src="images/icone_fleche-retour.png" alt="Haut de page"/></a>
+        </footer>
     </body>
 </html>
